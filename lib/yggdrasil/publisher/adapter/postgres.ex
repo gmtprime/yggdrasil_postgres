e0a9c07c77972f510f20ae55dad47e612ef5a0c9
Alexander de Sousa <alexander.jose.de.sousa@gmail.com>
2019-07-03 19:50:57 +0200

    Added new connections for publishers

--- lib/yggdrasil/publisher/adapter/postgres.ex
+++ lib/yggdrasil/publisher/adapter/postgres.ex
@@ -6,46 +6,44 @@ defmodule Yggdrasil.Publisher.Adapter.Postgres do
   Subscription to channel:
 
   ```
-  iex(2)> channel = %Yggdrasil.Channel{name: "pg_channel", adapter: :postgres}
-  iex(3)> Yggdrasil.subscribe(channel)
+  iex(1)> channel = [name: "pg_channel", adapter: :postgres]
+  iex(2)> Yggdrasil.subscribe(channel)
   :ok
-  iex(4)> flush()
+  iex(3)> flush()
   {:Y_CONNECTED, %Yggdrasil.Channel{name: "pg_channel", (...)}}
   ```
 
   Publishing message:
 
   ```
-  iex(5)> Yggdrasil.publish(channel, "foo")
+  iex(4)> Yggdrasil.publish(channel, "foo")
   :ok
   ```
 
   Subscriber receiving message:
 
   ```
-  iex(6)> flush()
+  iex(5)> flush()
   {:Y_EVENT, %Yggdrasil.Channel{name: "pg_channel", (...)}, "foo"}
   ```
 
   The subscriber can also unsubscribe from the channel:
 
   ```
-  iex(7)> Yggdrasil.unsubscribe(channel)
+  iex(6)> Yggdrasil.unsubscribe(channel)
   :ok
-  iex(8)> flush()
+  iex(7)> flush()
   {:Y_DISCONNECTED, %Yggdrasil.Channel{name: "pg_channel", (...)}}
   ```
   """
   use Yggdrasil.Publisher.Adapter
-  use Connection
+  use GenServer
 
   require Logger
 
   alias Yggdrasil.Channel
+  alias Yggdrasil.Postgres.Connection.Generator, as: ConnectionGen
   alias Yggdrasil.Transformer
-
-  defstruct [:conn, :namespace]
-  alias __MODULE__, as: State
 
   ############
   # Client API
@@ -56,119 +54,62 @@ defmodule Yggdrasil.Publisher.Adapter.Postgres do
   """
   @spec start_link(term()) :: GenServer.on_start()
   @spec start_link(term(), GenServer.options()) :: GenServer.on_start()
-  @impl true
+  @impl Yggdrasil.Publisher.Adapter
   def start_link(namespace, options \\ [])
 
   def start_link(namespace, options) do
-    Connection.start_link(__MODULE__, namespace, options)
+    GenServer.start_link(__MODULE__, namespace, options)
   end
 
   @doc """
-  Stops a Postgres `publisher`.
+  Stops a Postgres `publisher`. Optionally, receives a stop `reason` (defaults
+  to `:normal`) and a `timeout` in milliseconds (defaults to `:infinity`).
   """
-  @spec stop(GenServer.server()) :: :ok
-  def stop(publisher)
-
-  def stop(publisher) do
-    GenServer.stop(publisher)
-  end
+  @spec stop(GenServer.name()) :: :ok
+  @spec stop(GenServer.name(), term()) :: :ok
+  @spec stop(GenServer.name(), term(), non_neg_integer() | :infinity) :: :ok
+  defdelegate stop(publisher, reason \\ :normal, timeout \\ :infinity),
+    to: GenServer
 
   @doc """
   Publishes a `message` in a `channel` using a `publisher` and optional and
   unused `options`.
   """
-  @spec publish(GenServer.server(), Channel.t(), term()) ::
+  @spec publish(GenServer.name(), Channel.t(), term()) ::
           :ok | {:error, term()}
-  @spec publish(GenServer.server(), Channel.t(), term(), Keyword.t()) ::
+  @spec publish(GenServer.name(), Channel.t(), term(), Keyword.t()) ::
           :ok | {:error, term()}
-  @impl true
+  @impl Yggdrasil.Publisher.Adapter
   def publish(publisher, channel, message, options \\ [])
 
   def publish(publisher, %Channel{} = channel, message, _options) do
-    Connection.call(publisher, {:publish, channel, message})
+    GenServer.call(publisher, {:publish, channel, message})
   end
 
   ####################
   # GenServer callback
 
-  @impl true
+  @impl GenServer
   def init(namespace) do
-    Process.flag(:trap_exit, true)
-    state = %State{namespace: namespace}
-    {:connect, :init, state}
+    {:ok, namespace}
   end
 
-  @impl true
-  def connect(_info, %State{namespace: namespace} = state) do
-    options = Yggdrasil.Postgres.Connection.postgres_options(%Yggdrasil.Postgres.Connection{namespace: namespace})
-    {:ok, conn} = Postgrex.start_link(options)
-    {:ok, %State{state | conn: conn}}
+  @impl GenServer
+  def handle_call({:publish, %Channel{} = channel, message}, _, namespace) do
+    result = send_message(namespace, channel, message)
+    {:reply, result, namespace}
   end
 
-  @impl true
-  def disconnect(_info, %State{conn: nil} = state) do
-    {:backoff, 5000, state}
-  end
+  #########
+  # Helpers
 
-  def disconnect(info, %State{conn: conn} = state) do
-    GenServer.stop(conn)
-    disconnect(info, %State{state | conn: nil})
-  end
-
-  @impl true
-  def handle_call(
-        {:publish, _, _},
-        _from,
-        %State{conn: nil} = state
-      ) do
-    {:reply, {:error, "Disconnected"}, state}
-  end
-
-  def handle_call(
-        {:publish, %Channel{name: name} = channel, message},
-        _from,
-        %State{conn: conn} = state
-      ) do
-    result =
-      with {:ok, encoded} <- Transformer.encode(channel, message),
-           {:ok, _} <- Postgrex.query(conn, "NOTIFY #{name}, '#{encoded}'", []),
-           do: :ok
-
-    {:reply, result, state}
-  end
-
-  def handle_call(_msg, _from, %State{} = state) do
-    {:noreply, state}
-  end
-
-  @impl true
-  def handle_info({:DOWN, _, :process, _pid, _reason}, %State{} = state) do
-    new_state = %State{state | conn: nil}
-    {:disconnect, :down, new_state}
-  end
-
-  def handle_info({:EXIT, _, _}, %State{} = state) do
-    new_state = %State{state | conn: nil}
-    {:disconnect, :exit, new_state}
-  end
-
-  @impl true
-  def terminate(reason, %State{conn: nil} = state) do
-    terminated(reason, state)
-  end
-
-  def terminate(reason, %State{conn: conn} = state) do
-    GenServer.stop(conn)
-    terminate(reason, %State{state | conn: nil})
-  end
-
-  defp terminated(reason, %State{} = _state) do
-    metadata = [error: reason]
-
-    Logger.debug(fn ->
-      "Terminated Postgres connection #{inspect(metadata)}"
-    end)
-
-    :ok
+  ##
+  # Sends a messages to PostgreSQL.
+  defp send_message(namespace, %Channel{name: name} = channel, message) do
+    with {:ok, encoded} <- Transformer.encode(channel, message),
+         {:ok, conn} <- ConnectionGen.get_connection(:publisher, namespace),
+         {:ok, _} <- Postgrex.query(conn, "NOTIFY #{name}, '#{encoded}'", []) do
+      :ok
+    end
   end
 end
